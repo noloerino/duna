d922b2fb25f1dfaef43fff3be450c94e49867809
noloerino <jhshi07@gmail.com>
2020-06-04 00:53:28 -0700

    [frontend] Bump dependencies and add linter

--- duna_frontend/www/index.js
+++ duna_frontend/www/index.js
@@ -1,5 +1,5 @@
-import {init, simulate} from "duna_frontend";
-import {memory} from "duna_frontend/duna_frontend_bg";
+import { init, simulate } from "duna_frontend";
+import { memory } from "duna_frontend/duna_frontend_bg";
 
 init();
 
@@ -7,16 +7,16 @@ const codeArea = document.getElementById("code");
 const stdout = document.getElementById("stdout");
 const exitCode = document.getElementById("exit-code");
 
-const getProgram = function() {
-    return codeArea.value;
-}
+const getProgram = function () {
+  return codeArea.value;
+};
 
-const assembleAndRunProgram = function() {
-    const program = getProgram();
-    const simResult = simulate(program);
-    exitCode.innerText = simResult.exit_code;
-    stdout.value = simResult.get_stdout();
+const assembleAndRunProgram = function () {
+  const program = getProgram();
+  const simResult = simulate(program);
+  exitCode.innerText = simResult.exit_code;
+  stdout.value = simResult.get_stdout();
 };
 
 const goButton = document.getElementById("go");
-goButton.onclick = e => assembleAndRunProgram();
+goButton.onclick = (e) => assembleAndRunProgram();
