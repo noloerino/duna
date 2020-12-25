7f975cc5ad2e3e8ea82e0d26f34f5c16e69bd7c8
noloerino <jhshi07@gmail.com>
2020-12-25 01:36:42 -0800

    Clean up unused files

--- src/program_state/datatypes/mod.rs
+++ src/program_state/datatypes/mod.rs
@@ -1,11 +1,7 @@
-// mod addr_widths;
 mod bitstr32;
 mod data_enum;
 mod data_value;
-// mod data_widths;
 
-// pub use addr_widths::*;
 pub use bitstr32::*;
 pub use data_enum::*;
 pub use data_value::*;
-// pub use data_widths::*;
